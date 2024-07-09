47381ea9e0fa6f189e5273a9811f4bf970eb95f0
Stas Ukolov <ukoloff@gmail.com>
2024-07-09 10:23:22 +0500

    Make GET request

--- test/index.js
+++ test/index.js
@@ -1,1 +1,10 @@
-console.log('Hello, world!!!')
+const http = require('node:http')
+
+http.get('http://localhost:5432', get)
+
+function get(res) {
+  console.log('GOT!')
+  console.log('Status:', res.statusCode)
+  console.log('Headers:', res.headers)
+  res.pipe(process.stdout)
+}
