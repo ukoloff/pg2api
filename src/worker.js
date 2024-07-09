bf5c6870775cc0fa54bfc73beed9b3d62f29d42d
Stas Ukolov <ukoloff@gmail.com>
2024-07-09 10:14:05 +0500

    Restrict to localhost

--- src/worker.js
+++ src/worker.js
@@ -3,4 +3,4 @@ const http = require('node:http')
 http.createServer((req, res) => {
   res.writeHead(200);
   res.end(`Hello, world[${process.pid}]!`);
-}).listen(5432)
+}).listen(5432, 'localhost')
